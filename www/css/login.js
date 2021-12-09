da6aacf3306bc42920ce44d96190d521b9a0f733
hcs-s3-mana <manaJasmineTan3@gmail.com>
2021-12-09 11:55:21 +0900

    ログイン画面のユーザ新規

--- www/css/login.js
+++ www/css/login.js
@@ -1,4 +1,5 @@
-
-$('.message a').click(function(){
-   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
-});
+// alert(0011)
+// $('.change').click(function(){
+//   alert(0012)
+//    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+// });
