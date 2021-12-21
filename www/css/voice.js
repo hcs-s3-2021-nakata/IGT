e6ae4b59c1fd4d897cef3ab4ff17fe8f6e17f28d
hcs-s3-2021-nakata <20193088-NakataYuki@hcs.ac.jp>
2021-12-21 12:27:17 +0900

    詳細とボイスと

---
+++ www/css/voice.js
@@ -0,0 +1,25 @@
+/* 萬年先生のボイス */
+function boy() {
+  audio = new Audio("voice/mr.mannen/mannen_boy.m4a");
+  audio.play();
+}
+function girl() {
+  audio = new Audio("voice/mr.mannen/mannen_girl.m4a");
+  audio.play();
+}
+function man_free() {
+  audio = new Audio("voice/mr.mannen/mannen_free.m4a");
+  audio.play();
+}
+function man_com() {
+  audio = new Audio("voice/mr.mannen/mannen_com.m4a");
+  audio.play();
+}
+function man_grad() {
+  audio = new Audio("voice/mr.mannen/mannen_grad.m4a");
+  audio.play();
+}
+function kokoro() {
+  audio = new Audio("voice/mr.mannen/mannen_kokoro.m4a");
+  audio.play();
+}
