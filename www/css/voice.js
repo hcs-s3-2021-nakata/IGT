b8d77ac103fb2747023872b8ce25248948111629
hcs-s3-2021-nakata <20193088-NakataYuki@hcs.ac.jp>
2021-12-23 12:30:27 +0900

    先生ボイス実装

--- www/css/voice.js
+++ www/css/voice.js
@@ -23,3 +23,59 @@ function kokoro() {
   audio = new Audio("voice/mr.mannen/mannen_kokoro.m4a");
   audio.play();
 }
+
+/* 権藤先生ボイス */
+function boko() {
+  audio = new Audio("voice/mr.gondo/gon_boko.m4a");
+  audio.play();
+}
+function mou() {
+  audio = new Audio("voice/mr.gondo/gon_mou.m4a");
+  audio.play();
+}
+function sei() {
+  audio = new Audio("voice/mr.gondo/gon_true.m4a");
+  audio.play();
+}
+function gon_com() {
+  audio = new Audio("voice/mr.gondo/gon_com.m4a");
+  audio.play();
+}
+function gon_grad() {
+  audio = new Audio("voice/mr.gondo/gon_grad.m4a");
+  audio.play();
+}
+function gon_free() {
+  audio = new Audio("voice/mr.gondo/gon_free.m4a");
+  audio.play();
+}
+
+/* 佐藤先生ボイス */
+function yatta() {
+  audio = new Audio("voice/mr.sato/sato_yatta.m4a");
+  audio.play();
+}
+function though() {
+  audio = new Audio("voice/mr.sato/sato_though.m4a");
+  audio.play();
+}
+function naru() {
+  audio = new Audio("voice/mr.sato/sato_naru.m4a");
+  audio.play();
+}
+function clam() {
+  audio = new Audio("voice/mr.sato/sato_clam.m4a");
+  audio.play();
+}
+function sato_com() {
+  audio = new Audio("voice/mr.sato/sato_com.m4a");
+  audio.play();
+}
+function sato_grad() {
+  audio = new Audio("voice/mr.sato/sato_grad.m4a");
+  audio.play();
+}
+function sato_free() {
+  audio = new Audio("voice/mr.sato/sato_free.m4a");
+  audio.play();
+}
