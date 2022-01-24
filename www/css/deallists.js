658d5ddfee6c0969ff385c918decf069a9f8f46f
hcs-s3-2021-nkt <20193029-nakatasota@hcs.ac.jp>
2022-01-24 10:31:08 +0900

    現状の変更全保存

--- www/css/deallists.js
+++ www/css/deallists.js
@@ -288,3 +288,138 @@ function g_link(objectId) {
 function t_link(objectId) {
     localStorage.setItem("dealflg", "trade");
 }
+
+
+
+// 取引履歴
+
+// 一覧のロード
+async function historys_load() {
+    console.log("give");
+    history_give_lists();
+    await wait(6);
+    console.log("trade");
+    history_trade_lists();
+}
+
+// 商品のリストを展開する
+async function history_give_lists() {
+    var Give = ncmb.DataStore("give");
+    var myId = getCurrentUserId();
+    var subquery1 = Give.equalTo("give_user_id", myId);
+    var subquery2 = Give.equalTo("take_user_id", myId);
+    Give.equalTo("deal_status", "")
+        .or([subquery1, subquery2])
+        .fetchAll()
+        .then(function (results) {
+            for (var i = results.length - g_cntSwitch - (g_cnt * 4); i > results.length - 4 * (g_cnt + 1); i--) {
+                var object = results[i];
+                var image_id = "g_image" + (i + 1);
+                var objectId = object.objectId;
+                // 商品の箱を追加していく
+                var itemlist_element = document.getElementById('itemlist_give');
+                itemlist_element.insertAdjacentHTML("beforebegin", '<div class="container" id="item_box"> <div class="container_parent"> <div class="container_left"> <img src="" id="' + image_id + '" width="200" height="200"/> </div> <div class="container_center"> <a id="item_name">' + object.item_name + '</a> </div> <div class="container_right"> <a  onclick="g_link()" href="dealDetail.html?' + objectId + '"> <i class="fas fa-angle-right size"></i></a></div></div></div>');
+                g_imgs[i] = object.item_image;
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+    cnt = g_cnt;
+    var flg = 0;
+    console.log("setImg(g)");
+    await wait(2);
+    setImg(flg);
+}
+
+// 商品名を全件取得(成立待ちのみ)
+async function history_give_search() {
+    var c_cnt = document.getElementsByClassName("container").length;
+    // 既存の要素を削除
+    for (var c = 0; c < c_cnt; c++) {
+        document.getElementById("item_box").remove();
+    }
+    suji.splice(0.1);
+    // 検索
+    var Give = ncmb.DataStore("give");
+    Give.equalTo("deal_status", "成立待ち")
+        .fetchAll()
+        .then(function (results) {
+            for (var i = 0; i < results.length; i++) {
+                var object = results[i];
+                var objectId = object.objectId;
+                // 配列に格納
+                resultName[i] = object.item_name;
+                resultObject[i] = object.objectId;
+                g_imgs[i] = object.item_image;
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+    // 関数を呼ぶ
+    cnt = g_cnt;
+    await wait(1);
+    pickup();
+}
+
+// 商品のリストを展開する
+async function history_trade_lists() {
+    var Trade = ncmb.DataStore("trade");
+    var myId = getCurrentUserId();
+    var subquery1 = Trade.equalTo("give_user_id", myId);
+    var subquery2 = Trade.equalTo("take_user_id", myId);
+    Trade.equalTo("deal_status", "取引成立")
+        .or([subquery1, subquery2])
+        .fetchAll()
+        .then(function (results) {
+            for (var i = results.length - t_cntSwitch - (t_cnt * 4); i > results.length - 4 * (t_cnt + 1); i--) {
+                var object = results[i];
+                var image_id = "t_image" + (i + 1);
+                var objectId = object.objectId;
+                // 商品の箱を追加していく
+                var itemlist_element = document.getElementById('itemlist_trade');
+                itemlist_element.insertAdjacentHTML("beforebegin", '<div class="container" id="item_box"> <div class="container_parent"> <div class="container_left"> <img src="" id="' + image_id + '" width="200" height="200"/> </div> <div class="container_center"> <a id="item_name">' + object.item_name + '</a> </div> <div class="container_right"> <a  onclick="t_link()" href="dealDetail.html?' + objectId + '"> <i class="fas fa-angle-right size"></i></a></div></div></div>');
+                t_imgs[i] = object.item_image;
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+    cnt = t_cnt;
+    var flg = 1;
+    console.log("setImg(t)");
+    await wait(1);
+    setImg(flg);
+}
+
+// 商品名を全件取得(成立待ちのみ)
+async function history_trade_search() {
+    var c_cnt = document.getElementsByClassName("container").length;
+    // 既存の要素を削除
+    for (var c = 0; c < c_cnt; c++) {
+        document.getElementById("item_box").remove();
+    }
+    suji.splice(0.1);
+    // 検索
+    var Trade = ncmb.DataStore("trade");
+    Trade.equalTo("deal_status", "成立待ち")
+        .fetchAll()
+        .then(function (results) {
+            for (var i = 0; i < results.length; i++) {
+                var object = results[i];
+                var objectId = object.objectId;
+                // 配列に格納
+                resultName[i] = object.item_name;
+                resultObject[i] = object.objectId;
+                t_imgs[i] = object.item_image;
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+    // 関数を呼ぶ
+    cnt = t_cnt;
+    await wait(1);
+    pickup();
+}
