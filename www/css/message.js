fae68edce4e9117540b4c09a56ae871090690f62
hcs-s3-2021-nkt <20193029-nakatasota@hcs.ac.jp>
2022-01-27 10:35:08 +0900

    通知機能　現在テスト段階

--- www/css/message.js
+++ www/css/message.js
@@ -5,22 +5,117 @@ var clientKey = "00902e576a265322a217a2b2706056ca26048d4bd4f9096744fdd7e565d0f37
 // SDK initialization.
 var ncmb = new NCMB(applicationKey, clientKey);
 var reader = new FileReader();
+// 必要なグローバル変数の定義
+var current_user_id = getCurrentUserId();
 
-// 商品のリストを展開する
-async function messages() {
-  var Message = ncmb.DataStore("message");
-  Message.equalTo("receive_user_id", "current_user_id")
-    .fetchAll()
-    .then(function (results) {
-      for (var i = results.length; i > 0; i--) {
-        var object = results[i];
-        var objectId = object.objectId;
-        // 商品の箱を追加していく
-        var messagelist_element = document.getElementById('itemlist');
-        messagelist_element.insertAdjacentHTML("beforebegin", ' この辺に通知の箱を追加 ');
-      }
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
+// 処理を一時停止する
+const wait = (sec) => { // タイマ
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, sec * 1000);
+    //setTimeout(() => {reject(new Error("エラー！"))}, sec*1000);
+  });
+};
+
+// 通知のリストを展開する
+function messages() {
+    console.log("function boot " + current_user_id);
+    var Message = ncmb.DataStore("message");
+    Message.equalTo("receive_user_id", current_user_id)
+        .fetchAll()
+        .then(async function (results) {
+            for (var i = results.length; i > 0; i--) {
+                console.log("get results");
+                // 必要な変数の定義
+                var object = results[i-1];
+                console.log(object);
+                var createDate = object.createDate;
+                var message = object.message;
+                var read_flag = object.read_flag;
+                // 既読未読の分類を行う
+                var readState = setReadState(read_flag);
+                // 日付の書き換えを行う
+                var new_createDate = dateRewrite(createDate);
+                await wait(0.1);
+                // 通知を表示する
+                var messagelist_element = document.getElementById('messagelist');
+                messagelist_element.insertAdjacentHTML("beforebegin", ' <li class="message"><div class="ef_img"><img class="message" src="img/message.png">' + new_createDate + ' ' + '<span id="readState">' + readState + '</span><br><div class="ef_div"><a><span>' + message + '</span></a></div></li> ');
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 }
+
+//カレントユーザーのID取得
+        function getCurrentUserId() {
+            // カレントユーザー情報の取得
+            var currentUser = ncmb.User.getCurrentUser();
+            console.log("currentUser:" + currentUser);
+            if (currentUser != null) {
+                console.log("ユーザー: " + currentUser.get("userName"));
+                var c_u_id = currentUser.get("objectId");
+                return c_u_id;
+            } else {
+                console.log("未ログインまたは取得に失敗");
+                return null;
+            }
+        }
+
+// 日付の書き換え関数
+function dateRewrite(createDate){
+    // DBから引き出したデータを日本時間に直す
+    var c_date = new Date(createDate).toLocaleString({ timeZone: 'Asia/Tokyo' });
+    // 余分な値を排除
+    var day = c_date.substring(0, c_date.indexOf(" "))
+    var time = new Date(createDate).toTimeString();
+    time = time.substring(0, 5);
+    console.log("日 "+day +"\n時 "+ time);
+    // 通知作成日が今日の場合分岐
+    var date = new Date().toLocaleString({ timeZone: 'Asia/Tokyo' });
+    date = date.substring(0, date.indexOf(" "));
+    if(day == date){
+        day = "今日";
+    }    
+    return day +" "+ time;
+}
+
+// 通知が既読かどうか分類する関数
+function setReadState(read_flag){
+    var readState = "";
+    if (read_flag == 0) { // 未読であれば
+        readState = "未読";
+    }
+    return readState;
+}
+
+// 通知を既読にする関数
+function kidoku(){
+    // 表示部分の改変
+    var readState = "";
+    var state = document.getElementById("readState");
+    state.innerHTML = readState;
+
+    // データベースの改変
+    // var Message = ncmb.DataStore("message");
+    // Message.equalTo("objectId", objectId)
+    //     .fetch()
+    //     .then(function (results) {
+    //         for (var i = results.length; i > 0; i--) {
+    //             // 必要な変数の定義
+    //             var object = results[i];
+    //             var createDate = object.createDate;
+    //             var message = object.message;
+    //             var read_flag = object.read_flag;
+    //             // 既読未読の分類を行う
+    //             var readState = setReadState(read_flag);
+    //             // 通知を表示する
+    //             var messagelist_element = document.getElementById('messagelist');
+    //             messagelist_element.insertAdjacentHTML("beforebegin", ' <li class="message"><div class="ef_img"><img class="message" src="img/message.png">' + createDate + ' ' + '<span id="readState">' + readState + '</span><br><div class="ef_div"><a><span>' + message + '</span></a></div></li> ');
+    //         }
+    //     })
+    //     .catch(function (err) {
+    //         console.log(err);
+    //     });
+}
+
+
